bf7c46e5b8ab981f04548ca0ad5aa70c86ce7342
ISMAEL SOUSA <ismael@t2s.com.br>
2022-12-15 18:03:05 -0300

    Updates

---
+++ precache-manifest.042ade0f98ea5ec8d95a41d4b1590b36.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "60383b2d2ef9853bd59ec94e596d2d4a",
+    "url": "/youtube-clone/index.html"
+  },
+  {
+    "revision": "ba66b9b551069a462a4d",
+    "url": "/youtube-clone/static/css/main.16dfed35.chunk.css"
+  },
+  {
+    "revision": "58061d182cade5bafd35",
+    "url": "/youtube-clone/static/js/2.ebafb313.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/youtube-clone/static/js/2.ebafb313.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ba66b9b551069a462a4d",
+    "url": "/youtube-clone/static/js/main.d85a49a1.chunk.js"
+  },
+  {
+    "revision": "8a09cd451cb85b60110f",
+    "url": "/youtube-clone/static/js/runtime-main.a270c926.js"
+  },
+  {
+    "revision": "18aa4bba6b8bc580c752b69ce8bac180",
+    "url": "/youtube-clone/static/media/bars.18aa4bba.svg"
+  },
+  {
+    "revision": "360daa9ced2c438a5049057da59d0086",
+    "url": "/youtube-clone/static/media/bell.360daa9c.svg"
+  },
+  {
+    "revision": "1a27bcc88ca6f42fb4c272a129555d5d",
+    "url": "/youtube-clone/static/media/camera.1a27bcc8.svg"
+  },
+  {
+    "revision": "1f3ba12642974652611d65658b3b498f",
+    "url": "/youtube-clone/static/media/home.1f3ba126.svg"
+  },
+  {
+    "revision": "c5006fe19b07680e0980890fb5320f31",
+    "url": "/youtube-clone/static/media/logo.c5006fe1.svg"
+  },
+  {
+    "revision": "180c74d4a1ac21b0e9a8d6bd6defda92",
+    "url": "/youtube-clone/static/media/menu.180c74d4.svg"
+  },
+  {
+    "revision": "363e63c4be307ae462a9f7aa956d283c",
+    "url": "/youtube-clone/static/media/search.363e63c4.svg"
+  },
+  {
+    "revision": "404a236cf74d8b98d54a1ef77e520889",
+    "url": "/youtube-clone/static/media/thumb.404a236c.webp"
+  },
+  {
+    "revision": "bf6d85f70577469d8d82b595901a1ea6",
+    "url": "/youtube-clone/static/media/thumb2.bf6d85f7.webp"
+  },
+  {
+    "revision": "4ad1971a2e48fabf98103f3daf9bd65d",
+    "url": "/youtube-clone/static/media/thumb2_anim.4ad1971a.webp"
+  },
+  {
+    "revision": "7e3813c53e69efccbf5230361bf59ba2",
+    "url": "/youtube-clone/static/media/thumb_anim.7e3813c5.webp"
+  }
+]);
